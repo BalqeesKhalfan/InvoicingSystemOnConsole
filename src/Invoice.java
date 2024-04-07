c70d1a1c20a3908e700b3c5916ec38c25e0f930f
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-04-07 09:50:50 +0400

    Update Base classes
    Delete not need classes and update classes Item and Invoice with add the properties

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,12 +1,89 @@
-import java.util.Date;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public class Invoice {
-     String customerFullName;
+    Integer invoiceNumber;
+    String customerName;
      String phoneNumber;
-    Date invoiceDate;
-    List<Item> items;
+     LocalDate invoiceDate;// to add the curante date
+     List<Item> items;
      double totalAmount;
-     double paidAmount;
+    double paidAmount;
      double balance;
+
+    public Invoice(Integer invoiceNumber, String customerName, String phoneNumber, LocalDate invoiceDate, List<Item> items, double totalAmount, double paidAmount, double balance) {
+        this.invoiceNumber = invoiceNumber;
+        this.customerName = customerName;
+        this.phoneNumber = phoneNumber;
+        this.invoiceDate = invoiceDate;
+        this.items = items;
+        this.totalAmount = totalAmount;
+        this.paidAmount = paidAmount;
+        this.balance = balance;
+    }
+
+    public Integer getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(Integer invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(LocalDate invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public double getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(double paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
--- src/Item.java
+++ src/Item.java
@@ -4,4 +4,48 @@ public class Item {
      double unitPrice;
     Integer quantity;
 
+    public Item(Integer itemId, String itemName, double unitPrice, Integer quantity) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    // method to calculate the amount 
+    public double calculateAmount() {
+        return unitPrice* quantity;
+    }
+
 }
