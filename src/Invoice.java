78d4dc8e7f51e9460b3cd26fb757c25630d69149
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-04-07 10:19:50 +0400

    Add Item Method
    create method to add new Item and other methods (calculate total amount , and update Paid amount )

--- src/Invoice.java
+++ src/Invoice.java
@@ -12,6 +12,8 @@ public class Invoice {
     double paidAmount;
      double balance;
 
+
+
     public Invoice(Integer invoiceNumber, String customerName, String phoneNumber, LocalDate invoiceDate, List<Item> items, double totalAmount, double paidAmount, double balance) {
         this.invoiceNumber = invoiceNumber;
         this.customerName = customerName;
@@ -24,6 +26,7 @@ public class Invoice {
     }
 
     public Integer getInvoiceNumber() {
+
         return invoiceNumber;
     }
 
@@ -85,5 +88,41 @@ public class Invoice {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    // method to add new Item
+    public void addItem(Item item, Integer quantity) {
+        items.add(item);
+        quantity += quantity;
+        this.totalAmount += item.calculateAmount() * quantity;
+        this.balance = this.totalAmount - this.paidAmount;
+    }
+    // method to calculate total mount
+    public double calculateTotalAmount() {
+        double total = 0.0;
+        for (Item item : items) {
+            total += item.calculateAmount();
+        }
+        return total;
+    }
+    //method to update paid amount
+    public void updatePaidAmount(double amount) {
+        if (amount >= 0) {
+            this.paidAmount += amount;
+            this.balance = this.totalAmount - this.paidAmount;
+        } else {
+            System.out.println("Error: Paid amount cannot be negative.");
+        }
+    }
+
+    // method to mark Invoice paid 
+    public void markInvoicePaid() {
+        if (this.balance == 0.0) {
+            System.out.println("Invoice already paid.");
+        } else {
+            this.paidAmount = this.totalAmount;
+            this.balance = 0.0;
+            System.out.println("Invoice marked as paid.");
+        }
     }
 }
