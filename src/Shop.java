258ed77228796f9e370a9a72cdf318fc1b61e895
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-04-08 10:26:42 +0400

    Add methos of mage Item and Invoces

--- src/Shop.java
+++ src/Shop.java
@@ -23,15 +23,51 @@ public class Shop {
     public void setShopName(String shopName) {
         this.shopName = shopName;
     }
-    
+
     public void setInvoiceHeader(String tel, String fax, String email, String website) {
         invoiceHeader.put("Telephone", tel);
         invoiceHeader.put("Fax", fax);
         invoiceHeader.put("Email", email);
         invoiceHeader.put("Website", website);
     }
+  //
+  // Add Methods Of  managing items
+    //add Item
+  public void addItem(Item item) {
+      items.add(item);
+  }
+   //
+    public List<Item> getItems() {
+        return items;
+    }
+    //delete an Item
+    public void deleteItem(int itemId) {
+        items.removeIf(item -> item.getItemId() == itemId);
+    }
+    // update Item Price
+    public void changeItemPrice(int itemId, double newPrice) {
+        for (Item item : items) {
+            if (item.getItemId() == itemId) {
+                item.setUnitPrice(newPrice);
+                break;
+            }
+        }
+    }
+    // method of reporting all Items
+    public void reportAllItems() {
+        System.out.println("All Shop Items:");
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+    //methods of Managing invoices
+    public void addInvoice(Invoice invoice) {
+        invoices.add(invoice);
+    }
 
-
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
 
 
 
